6da47db9a97a207625da8258562760259427b9ff
Fernando Moro Hernández <fernando@MacBook-Pro-de-Fernando.local>
2015-10-20 21:50:09 +0200

    Moved MemoryDaos to /rest/data/models/daos/memory/

---
+++ src/main/java/es/upm/miw/voting/rest/data/models/daos/memory/DaoFactoryMemory.java
@@ -0,0 +1,27 @@
+package es.upm.miw.voting.rest.data.models.daos.memory;
+public class DaoFactoryMemory extends DaoFactory {
+
+    private ThemeDao themeDao;
+
+    private VoteDao voteDao;
+
+    public DaoFactoryMemory() {
+    }
+
+    @Override
+    public ThemeDao getThemeDao() {
+        if (themeDao == null) {
+            themeDao = new ThemeMemoryDao();
+        }
+        return themeDao;
+    }
+
+    @Override
+    public VoteDao getVoteDao() {
+        if (voteDao == null) {
+            voteDao = new VoteMemoryDao();
+        }
+        return voteDao;
+    }
+
+}
