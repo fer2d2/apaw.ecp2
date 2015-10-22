f9ed22cf7cbaf3b37e61429bade8c226c9a98c9d
Fernando Moro Hernández <morohernandez.fernando@gmail.com>
2015-10-22 17:11:59 +0200

    Added Dispatcher stub

---
+++ src/main/java/es/upm/miw/voting/web/presentation/frontController/Dispatcher.java
@@ -0,0 +1,16 @@
+package es.upm.miw.voting.web.presentation.frontController;
+
+import es.upm.miw.voting.util.http.HttpRequest;
+import es.upm.miw.voting.util.http.HttpResponse;
+
+public class Dispatcher {
+
+    public void doGet(HttpRequest request, HttpResponse response) {
+        
+    }
+
+    public void doPost(HttpRequest request, HttpResponse response) {
+        
+    }
+
+}
