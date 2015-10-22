042f9ee60d7774f079c968854289656a9c2ee92d
Fernando Moro Hernández <morohernandez.fernando@gmail.com>
2015-10-22 21:15:46 +0200

    Implemented views

--- src/main/java/es/upm/miw/voting/web/presentation/views/ThemeManagerView.java
+++ src/main/java/es/upm/miw/voting/web/presentation/views/ThemeManagerView.java
@@ -1,13 +1,16 @@
 package es.upm.miw.voting.web.presentation.views;
 
+import java.util.List;
+
+import es.upm.miw.voting.util.IO.OutputGenerator;
 import es.upm.miw.voting.web.presentation.models.Model;
 
 public class ThemeManagerView implements View {
 
     @Override
     public void show(Model model) {
-        // TODO Auto-generated method stub
-
+        OutputGenerator outputGenerator = new OutputGenerator();
+        outputGenerator.printList("Temas", model.get("themes"));
     }
 
 }
--- src/main/java/es/upm/miw/voting/web/presentation/views/VotingView.java
+++ src/main/java/es/upm/miw/voting/web/presentation/views/VotingView.java
@@ -1,12 +1,16 @@
 package es.upm.miw.voting.web.presentation.views;
 
+import java.util.List;
+
+import es.upm.miw.voting.util.IO.OutputGenerator;
 import es.upm.miw.voting.web.presentation.models.Model;
 
 public class VotingView implements View {
 
     @Override
     public void show(Model model) {
-
+        OutputGenerator outputGenerator = new OutputGenerator();
+        outputGenerator.printListWithSubtitle("Temas", "ThemeTransfer", model.get("votes"));
     }
 
 }
