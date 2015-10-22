66cbd81bbc75d664eb6d5f7e99e9e4eb9f8c3638
Fernando Moro Hernández <morohernandez.fernando@gmail.com>
2015-10-22 20:02:50 +0200

    Added ThemeTransfer and VoteTransfer

---
+++ src/main/java/es/upm/miw/voting/rest/business/views/ThemeTransfer.java
@@ -0,0 +1,22 @@
+package es.upm.miw.voting.rest.business.views;
+
+public class ThemeTransfer {
+
+    public String themeName;
+    public double themeAverage;
+    
+    public ThemeTransfer(String themeName, double themeAverage) {
+        super();
+        this.themeName = themeName;
+        this.themeAverage = themeAverage;
+    }
+
+    public String getThemeName() {
+        return themeName;
+    }
+
+    public double getThemeAverage() {
+        return themeAverage;
+    }
+
+}
---
+++ src/main/java/es/upm/miw/voting/rest/business/views/VoteTransfer.java
@@ -0,0 +1,22 @@
+package es.upm.miw.voting.rest.business.views;
+
+public class VoteTransfer {
+
+    private String themeName;
+    private int voteValue;
+    
+    public VoteTransfer(String themeName, int voteValue) {
+        this.themeName = themeName;
+        this.voteValue = voteValue;
+    }
+
+    public String getThemeName() {
+        return themeName;
+    }
+
+    public int getVoteValue() {
+        return voteValue;
+    }
+    
+
+}
